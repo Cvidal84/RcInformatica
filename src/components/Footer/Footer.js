cce727bf94fc52d6e320d495e44bf765bdffcf42
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-07-13 11:03:53 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,5 +8,9 @@ ${Button("/icons/facebook.png", "Facebook","https://www.facebook.com/rc.informat
 ${Button("/icons/whatsapp.png", "Whatsapp","https://wa.me/34698986560")}
 ${Button("/icons/in.png", "LinkedIn","https://www.linkedin.com/company/rcinformatica/")}
 </div>
+<div class="info">
+<p>RcInformática C/Colón nº 44 Callosa d'en Sarriá (03510) Alicante</p>
+<p>96 588 08 80</p>
 <p>&copy; 2025 RcInformática. Todos los derechos reservados.</p>
+</div>
 `;
