f5f5ea189be3db4ce8ca88122cd0d683f1602134
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-12-03 23:19:58 +0100

    firebase apikey connect

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI",
+  apiKey: "AIzaSyD7Qa23-ZoKfXuD-M6FV_Sd-way2FVcQmY",
   authDomain: "rcinformatica-d8c51.firebaseapp.com",
   projectId: "rcinformatica-d8c51",
-  storageBucket: "rcinformatica-d8c51.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUI",
-  appId: "TU_APP_ID_AQUI",
+  storageBucket: "rcinformatica-d8c51.firebasestorage.app",
+  messagingSenderId: "660728428452",
+  appId: "1:660728428452:web:9c30f3d897e9de0a6bb331",
+  measurementId: "G-MB3R80RC2W",
 };
 
 // Initialize Firebase
